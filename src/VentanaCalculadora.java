69a9b2ba972fe88c5ad714e15fa7a66a0a35bb5a
MELANI <melani.molina@epn.edu.ec>
2023-01-21 19:39:04 -0500

    segunda

--- src/VentanaCalculadora.java
+++ src/VentanaCalculadora.java
@@ -1,15 +1,14 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VentanaCalculadora {
-    private JButton xButton;
-    private JButton button2;
-    private JButton button3;
-    private JButton button1;
-    private JButton xButton2;
-    private JButton button5;
-    private JButton button6;
-    private JButton ACButton;
-    private JButton button8;
+    private JButton Cerrar_botton;
+    private JButton button_max;
+    private JButton button_Suma;
+    private JButton Button_Multiplicacion;
+    private JButton button_Divisi;
+    private JButton button_Resta;
     private JButton a9Button;
     private JButton a7Button;
     private JButton a8Button;
@@ -19,11 +18,105 @@ public class VentanaCalculadora {
     private JButton a1Button;
     private JButton a2Button;
     private JButton a3Button;
-    private JButton OFButton;
-    private JButton DELButton;
-    private JButton button18;
+    private JButton ONEncender;
+    private JButton a0Button;
+    private JButton coma;
+    private JPanel panel1;
+    private JLabel Mostrar_Resultado;
+    private JLabel texto_log;
+    private JPanel panel_prin;
+    private JButton button1;
+    private JButton ACButton;
+    private JButton button3;
+
+    public VentanaCalculadora() {
+
+        a0Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+        a2Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+        a3Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+        a4Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        a5Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+        a6Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+        a7Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+        a8Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+        a9Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
+    }
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("VentanaCalculadora");
+        frame.setContentPane(new VentanaCalculadora().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
